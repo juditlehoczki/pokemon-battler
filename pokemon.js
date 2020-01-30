1473817df39afd4b230640161d68722ebed02490
juditlehoczki <lehoczki.judit@gmail.com>
2020-01-30 14:46:56 +0000

    Pokemon constructor methods added and tested

--- pokemon.js
+++ pokemon.js
@@ -4,7 +4,44 @@ function Pokemon(newName, newHealth, newDamage, newSound, newMove, newType) {
   this.damage = newDamage;
   this.sound = newSound;
   this.move = newMove;
-  this.type = newType || 'normal';
+  this.type = newType || "normal";
 }
+
+Pokemon.prototype.talk = function() {
+  return this.sound;
+};
+
+Pokemon.prototype.useYourMove = function() {
+  return this.move;
+};
+
+//Creating a few Pokemons to catch
+const pikachu = new Pokemon(
+  "Pikachu",
+  100,
+  10,
+  "Pika pika!",
+  "Mega Punch",
+  "grass"
+);
+const bulbasaur = new Pokemon(
+  "Bulbasaur",
+  90,
+  8,
+  "Bulba bulba!",
+  "Mega Kick",
+  "water"
+);
+const charmander = new Pokemon(
+  "Charmander",
+  95,
+  7,
+  "Char char!",
+  "Jump",
+  "fire"
+);
+const squirtle = new Pokemon("Squirtle", 85, 12, "Squirt squirt!", "Duck");
+
+const pokemonsToCatch = [pikachu, bulbasaur, charmander, squirtle];
 
 module.exports = Pokemon;
--- spec/pokemon-spec.js
+++ spec/pokemon-spec.js
@@ -1,44 +1,75 @@
-const {
-  expect
-} = require("chai");
-const {
-  spy
-} = require("sinon");
+const { expect } = require("chai");
+const { spy } = require("sinon");
 const Pokemon = require("../pokemon.js");
 
 describe("Pokemon", () => {
-  it("has a name property when newName argument passed", () => {
-    const poke = new Pokemon("Pikachu");
-    expect(poke.name).to.equal("Pikachu");
-  });
+  describe("Pokemon Properties", () => {
+    it("has a name property when newName argument passed", () => {
+      const poke = new Pokemon("Pikachu");
+      expect(poke.name).to.equal("Pikachu");
+    });
 
-  it("has a health property when newHealth argument passed", () => {
-    const poke = new Pokemon("Pikachu", 100);
-    expect(poke.health).to.equal(100);
-  });
+    it("has a health property when newHealth argument passed", () => {
+      const poke = new Pokemon("Pikachu", 100);
+      expect(poke.health).to.equal(100);
+    });
 
-  it("has a damage property when newDamage argument passed", () => {
-    const poke = new Pokemon("Pikachu", 100, 10);
-    expect(poke.damage).to.equal(10);
-  });
+    it("has a damage property when newDamage argument passed", () => {
+      const poke = new Pokemon("Pikachu", 100, 10);
+      expect(poke.damage).to.equal(10);
+    });
 
-  it("has a sound property when newSound argument passed", () => {
-    const poke = new Pokemon("Pikachu", 100, 10, 'Pika pika!');
-    expect(poke.sound).to.equal('Pika pika!');
-  });
+    it("has a sound property when newSound argument passed", () => {
+      const poke = new Pokemon("Pikachu", 100, 10, "Pika pika!");
+      expect(poke.sound).to.equal("Pika pika!");
+    });
 
-  it("has a move property when newMove argument passed", () => {
-    const poke = new Pokemon("Pikachu", 100, 10, 'Pika pika!', 'Mega Punch');
-    expect(poke.move).to.equal('Mega Punch');
-  });
+    it("has a move property when newMove argument passed", () => {
+      const poke = new Pokemon("Pikachu", 100, 10, "Pika pika!", "Mega Punch");
+      expect(poke.move).to.equal("Mega Punch");
+    });
 
-  it("has a type property which defaults to normal when no argument passed ", () => {
-    const poke = new Pokemon("Pikachu", 100, 10, 'Pika pika!', 'Mega Punch');
-    expect(poke.type).to.equal('normal');
-  });
+    it("has a type property which defaults to normal when no argument passed ", () => {
+      const poke = new Pokemon("Pikachu", 100, 10, "Pika pika!", "Mega Punch");
+      expect(poke.type).to.equal("normal");
+    });
 
-  it("has a type property which takes newType when argument passed ", () => {
-    const poke = new Pokemon("Pikachu", 100, 10, 'Pika pika!', 'Mega Punch', 'grass');
-    expect(poke.type).to.equal('grass');
+    it("has a type property which takes newType when argument passed ", () => {
+      const poke = new Pokemon(
+        "Pikachu",
+        100,
+        10,
+        "Pika pika!",
+        "Mega Punch",
+        "grass"
+      );
+      expect(poke.type).to.equal("grass");
+    });
+  });
+  describe("Pokemon Methods", () => {
+    it("has a talk method that returns sound", () => {
+      const poke = new Pokemon(
+        "Pikachu",
+        100,
+        10,
+        "Pika pika!",
+        "Mega Punch",
+        "grass"
+      );
+      const pokeTalk = poke.talk();
+      expect(pokeTalk).to.equal("Pika pika!");
+    });
+    it("has a useYourMove method that returns move", () => {
+      const poke = new Pokemon(
+        "Pikachu",
+        100,
+        10,
+        "Pika pika!",
+        "Mega Punch",
+        "grass"
+      );
+      const pokeMove = poke.useYourMove();
+      expect(pokeMove).to.equal("Mega Punch");
+    });
   });
 });
