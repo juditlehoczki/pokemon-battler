1becdcbf908ddbe2b8861ba6791773a53b99af19
juditlehoczki <lehoczki.judit@gmail.com>
2020-02-01 20:26:06 +0000

    updated the properties on Battle class

--- battle.js
+++ battle.js
@@ -1,16 +1,29 @@
-const Pokemon = require("./pokemon.js");
+const {
+  Pokemon,
+  Bulbasaur,
+  Charmander,
+  Squirtle,
+  Pidgey
+} = require("./pokemon.js");
 const Trainer = require("./pokemon.js");
 
 class Battle {
-  constructor(newTrainer1, newTrainer2) {
+  constructor(newTrainer1, newTrainer2, n, m) {
     this.trainer1 = newTrainer1;
     this.trainer2 = newTrainer2;
-    this.pokeToFight1 = newTrainer1.storage[0];
-    this.pokeToFight2 = newTrainer2.storage[0];
-    this.poke1health = this.pokeToFight1.health;
-    this.poke2health = this.pokeToFight2.health;
-    this.poke1damage = this.pokeToFight1.damage;
-    this.poke2damage = this.pokeToFight2.damage;
+    this.pokeToFight1 = newTrainer1.bag[n || 0];
+    this.pokeToFight2 = newTrainer2.bag[m || 0];
+    this.p1health = this.pokeToFight1.health;
+    this.p2health = this.pokeToFight2.health;
+    this.p1damage = this.pokeToFight1.damage;
+    this.p2damage = this.pokeToFight2.damage;
+    this.p1sound = this.pokeToFight1.sound;
+    this.p2sound = this.pokeToFight2.sound;
+    this.p1move = this.pokeToFight1.move;
+    this.p2move = this.pokeToFight2.move;
+    this.p1type = this.pokeToFight1.type;
+    this.p2type = this.pokeToFight2.type;
+    this.whoIsNext = 1;
   }
 
   fight() {}
--- spec/battle-spec.js
+++ spec/battle-spec.js
@@ -1,113 +1,76 @@
-const {
-  expect
-} = require("chai");
-const {
-  spy
-} = require("sinon");
+const { expect } = require("chai");
 const Battle = require("../battle.js");
-const Pokemon = require("../pokemon.js");
+const {
+  Pokemon,
+  Bulbasaur,
+  Charmander,
+  Squirtle,
+  Pidgey
+} = require("../pokemon.js");
 const Trainer = require("../trainer.js");
 
-describe.only("Battle", () => {
+describe("Battle", () => {
   describe("Battle's properties", () => {
     it("has a trainer1 and trainer2 property when newTrainer1 and newTrainer2 arguments passed", () => {
       const judit = new Trainer("Judit");
       const steve = new Trainer("Steve");
-      const pikachu = new Pokemon(
-        "Pikachu",
-        100,
-        10,
-        "Pika pika!",
-        "Mega Punch",
-        "grass"
-      );
-      const bulbasaur = new Pokemon(
-        "Bulbasaur",
-        90,
-        8,
-        "Bulba bulba!",
-        "Mega Kick",
-        "water"
-      );
-      judit.catch(pikachu);
-      steve.catch(bulbasaur);
+      const squirtle = new Squirtle();
+      const pidgey = new Pidgey();
+      judit.catch(squirtle);
+      steve.catch(pidgey);
       const battle = new Battle(judit, steve);
       expect(battle.trainer1).to.equal(judit);
       expect(battle.trainer2).to.equal(steve);
     });
-    it("has a trainPoke1 property with trainer1's pokemon", () => {
+    it("has a trainPoke1 and trainPoke2 property with trainer1 and trainer2's pokemons", () => {
       const judit = new Trainer("Judit");
       const steve = new Trainer("Steve");
-      const pikachu = new Pokemon(
-        "Pikachu",
-        100,
-        10,
-        "Pika pika!",
-        "Mega Punch",
-        "grass"
-      );
-      const bulbasaur = new Pokemon(
-        "Bulbasaur",
-        90,
-        8,
-        "Bulba bulba!",
-        "Mega Kick",
-        "water"
-      );
-      judit.catch(pikachu);
-      steve.catch(bulbasaur);
+      const squirtle = new Squirtle();
+      const pidgey = new Pidgey();
+      judit.catch(squirtle);
+      steve.catch(pidgey);
       const battle = new Battle(judit, steve);
-      expect(battle.pokeToFight1).to.equal(pikachu);
-      expect(battle.pokeToFight2).to.equal(bulbasaur);
+      expect(battle.pokeToFight1).to.equal(squirtle);
+      expect(battle.pokeToFight2).to.equal(pidgey);
     });
-    it("stores the Pokemons' health value", () => {
+    it("stores the Pokemons' health, damage, sound, move and and type values", () => {
       const judit = new Trainer("Judit");
       const steve = new Trainer("Steve");
-      const pikachu = new Pokemon(
-        "Pikachu",
-        100,
-        10,
-        "Pika pika!",
-        "Mega Punch",
-        "grass"
-      );
-      const bulbasaur = new Pokemon(
-        "Bulbasaur",
-        90,
-        8,
-        "Bulba bulba!",
-        "Mega Kick",
-        "water"
-      );
-      judit.catch(pikachu);
-      steve.catch(bulbasaur);
+      const bulbasaur = new Bulbasaur();
+      const charmander = new Charmander();
+      judit.catch(bulbasaur);
+      steve.catch(charmander);
       const battle = new Battle(judit, steve);
-      expect(battle.poke1health).to.equal(100);
-      expect(battle.poke2health).to.equal(90);
+      expect(battle.p1health).to.equal(800);
+      expect(battle.p2health).to.equal(780);
+      expect(battle.p1damage).to.equal(100);
+      expect(battle.p2damage).to.equal(104);
+      expect(battle.p1sound).to.equal("Bulbasaur!");
+      expect(battle.p2sound).to.equal("Charmander!");
+      expect(battle.p1move).to.equal("Vine Whip");
+      expect(battle.p2move).to.equal("Fire Punch");
+      expect(battle.p1type).to.equal("grass");
+      expect(battle.p2type).to.equal("fire");
+    });
+    it("has a whoIsNext property that defaults to 1", () => {
+      const judit = new Trainer("Judit");
+      const steve = new Trainer("Steve");
+      const bulbasaur = new Bulbasaur();
+      const charmander = new Charmander();
+      judit.catch(bulbasaur);
+      steve.catch(charmander);
+      const battle = new Battle(judit, steve);
+      expect(battle.whoIsNext).to.equal(1);
     });
   });
   describe("Battle's method", () => {
     it("has a fight method which is a function", () => {
       const judit = new Trainer("Judit");
       const steve = new Trainer("Steve");
-      const pikachu = new Pokemon(
-        "Pikachu",
-        100,
-        10,
-        "Pika pika!",
-        "Mega Punch",
-        "grass"
-      );
-      const bulbasaur = new Pokemon(
-        "Bulbasaur",
-        90,
-        8,
-        "Bulba bulba!",
-        "Mega Kick",
-        "water"
-      );
-      judit.catch(pikachu);
-      steve.catch(bulbasaur);
+      const bulbasaur = new Bulbasaur();
+      const charmander = new Charmander();
+      judit.catch(bulbasaur);
+      steve.catch(charmander);
       const battle = new Battle(judit, steve);
       expect(battle.fight).to.be.an("function");
     });
