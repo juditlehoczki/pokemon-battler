2c5c0eb22295a5d0a47d4b124d86c5e1c94b9d91
juditlehoczki <lehoczki.judit@gmail.com>
2020-02-01 20:26:49 +0000

    updated tests based on child classes

--- spec/trainer-spec.js
+++ spec/trainer-spec.js
@@ -1,11 +1,12 @@
-const {
-  expect
-} = require("chai");
-const {
-  spy
-} = require("sinon");
+const { expect } = require("chai");
 const Trainer = require("../trainer.js");
-const Pokemon = require("../pokemon.js");
+const {
+  Pokemon,
+  Bulbasaur,
+  Charmander,
+  Squirtle,
+  Pidgey
+} = require("../pokemon.js");
 
 describe("Trainer", () => {
   describe("Trainer properties", () => {
@@ -14,19 +15,14 @@ describe("Trainer", () => {
       expect(trainer.name).to.equal("Steve");
     });
 
-    it("has a storage array", () => {
+    it("has a bag array which defaults as empty array", () => {
       const trainer = new Trainer("Steve");
-      expect(trainer.storage).to.eql([]);
+      expect(trainer.bag).to.eql([]);
     });
   });
 
   describe("Trainer methods", () => {
-    it("has a catch method", () => {
-      const trainer = new Trainer("Steve");
-      expect(trainer.name).to.equal("Steve");
-    });
-    it("it has a catch method which pushes pokemon to trainers storage array", () => {
-      // ArrangeActAssert
+    it("it has a catch method which pushes a pokemon to trainer's bag array", () => {
       const pokemon = new Pokemon(
         "Pikachu",
         100,
@@ -34,121 +30,10 @@ describe("Trainer", () => {
         "Pika pika!",
         "Mega Punch",
         "grass"
-      );
-      const trainer = new Trainer("Steve");
-      const trainer2 = new Trainer("Steve");
-
-      trainer.catch(pokemon);
-      // trainer2.catch(pokemon);
-
-      expect(trainer.storage[0].name).to.equal("Pikachu");
-    });
-    it("it has a catch method that returns the pokemon sound  ", () => {
-      // ArrangeActAssert
-      const pokemon = new Pokemon(
-        "Pikachu",
-        100,
-        10,
-        "Pika pika!",
-        "Mega Punch",
-        "grass"
-      );
-      const trainer = new Trainer("Steve");
-      const catchPoke = trainer.catch(pokemon);
-      expect(catchPoke).to.equal("Pika pika!");
-    });
-
-    it("it has a catch method that returns the pokemon sound  ", () => {
-      // ArrangeActAssert
-      const pokemon = new Pokemon(
-        "Pikachu",
-        100,
-        10,
-        "Pika pika!",
-        "Mega Punch",
-        "grass"
-      );
-      const pokemon2 = new Pokemon(
-        "Bulbasaur",
-        90,
-        8,
-        "Bulba bulba!",
-        "Mega Kick",
-        "water"
       );
       const trainer = new Trainer("Steve");
       trainer.catch(pokemon);
-      trainer.catch(pokemon2);
-      expect(trainer.storage.length).to.equal(2);
-    });
-
-    it("it has a catch method that returns the pokemon sound  ", () => {
-      // ArrangeActAssert
-      const pokemon = new Pokemon(
-        "Pikachu",
-        100,
-        10,
-        "Pika pika!",
-        "Mega Punch",
-        "grass"
-      );
-      const pokemon2 = new Pokemon(
-        "Bulbasaur",
-        90,
-        8,
-        "Bulba bulba!",
-        "Mega Kick",
-        "water"
-      );
-      const pokemon3 = new Pokemon(
-        "Charmander",
-        95,
-        7,
-        "Char char!",
-        "Jump",
-        "fire"
-      );
-      const pokemon4 = new Pokemon(
-        "Pikachu2",
-        100,
-        10,
-        "Pika pika2!",
-        "Mega Punch",
-        "grass"
-      );
-      const pokemon5 = new Pokemon(
-        "Bulbasaur",
-        90,
-        8,
-        "Bulba bulba!",
-        "Mega Kick",
-        "water"
-      );
-      const pokemon6 = new Pokemon(
-        "Charmander",
-        95,
-        7,
-        "Char char!",
-        "Jump",
-        "fire"
-      );
-      const pokemon7 = new Pokemon(
-        "Charmander",
-        95,
-        7,
-        "Char char!",
-        "Jump",
-        "fire"
-      );
-      const trainer = new Trainer("Steve");
-      trainer.catch(pokemon);
-      trainer.catch(pokemon2);
-      trainer.catch(pokemon3);
-      trainer.catch(pokemon4);
-      trainer.catch(pokemon5);
-      trainer.catch(pokemon6);
-      trainer.catch(pokemon7);
-      expect(trainer.storage.length).to.equal(6);
+      expect(trainer.bag[0].name).to.equal("Pikachu");
     });
   });
 });
